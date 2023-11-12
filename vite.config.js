132be64663b4e959d838cb2bc24c4cd1ee0bc05a
PabloC1981 <pablocourroux@gmail.com>
2023-11-12 00:51:58 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Clase3_Arg_Prog_React/"
+  base: "/Clase3_APR/"
 })
